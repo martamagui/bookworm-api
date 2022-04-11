aa8e7dec3cb476dca04fd30f9e65496c5ce2688a
Marta Molina <marta.molina@live.u-tad.com>
2022-04-11 15:09:23 +0200

    WIP-(Chat gets)

--- bookworm-api/controllers/review/get/chatGet.ts
+++ bookworm-api/controllers/review/get/chatGet.ts
@@ -1,3 +1,34 @@
 import { Request, Response } from "express";
 //Internal
 import Chat from "../../../models/Chat";
+
+class ChatGetController {
+  public async byId(req: Request, res: Response) {
+    try {
+      const chat = await Chat.findById(req.params.chatId);
+      if (chat) {
+        return res.status(200).json(chat);
+      }
+      return res.status(404).json({ message: "Chat not found" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: "Error. Check console log." });
+    }
+  }
+  public async findChatsByUserId(req: Request, res: Response) {
+    try {
+      const chatList = await Chat.find({
+        users: { $all: [req.query.userIds] },
+      });
+      if (chatList) {
+        return res.status(200).json(chatList);
+      }
+      return res.status(404).json({ message: "Chat not found" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: "Error. Check console log." });
+    }
+  }
+}
+
+export default new ChatGetController();
